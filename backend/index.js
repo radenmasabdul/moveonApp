a017cca5faea11146ad8896359d0d6be5b0ea827
radenmasabdul <abdul.tamsis@gmail.com>
2024-07-29 21:32:47 +0700

    connection databases

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
-const express = require('express')
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 const app = express()
 const port = 3000
 
+//endpoint
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
@@ -9,3 +15,15 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Aplikasi berjalan di port ${port}`)
 })
+
+//connection database
+async function main() {
+    try {
+        await mongoose.connect(process.env.DATABASE);
+        console.log('Database connected');
+    } catch (error) {
+        console.log('Failed to connect to the database:', error)
+    }
+}
+
+main()
