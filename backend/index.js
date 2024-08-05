7d22202e2850dfd64d457914d18e3f076fae72a5
radenmasabdul <abdul.tamsis@gmail.com>
2024-08-06 00:09:56 +0700

    add cors

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
 const port = 3000
+
+app.use(cors())
 
 //endpoint
 app.get('/', (req, res) => {
